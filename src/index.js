574fa34ff002c0f238a7ad45a78ffa8073f0ecef
David Romero <dromerosalem@gmail.com>
2020-08-14 20:13:31 +0100

    rendering successfully

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import MainPage from "./components/MainPage.js";
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 const App = () => (
+
+  
   <BrowserRouter>
     <Switch>
-      <Route exact path='/main' component={MainPage}></Route>
+      <Route exact path='/' component={MainPage}></Route>
     </Switch>
   </BrowserRouter>
 )
