5e833654d9a16b9f88517cd99f40e8b2429ef2a5
David Romero <dromerosalem@gmail.com>
2020-08-15 20:17:54 +0100

    browser margin fix

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -6,7 +6,9 @@ class MainPage extends Component {
     render() {
         return (
             <div className='main-page-body'>
-              <h1>Welcome to the Rick and Morty Universe (all the universese with the inifinites Ricks and Morties included)</h1>
+              <div className='intro'>
+                <h1>Welcome to the Rick and Morty Universe (all the universese with the inifinites Ricks and Morties included)</h1>
+              </div>
                 <div className='main-page-main'></div>
             </div>
 
