e7a2da64430734b97c8a218d9cd6cb0b6f6f9362
David Romero <dromerosalem@gmail.com>
2020-08-22 00:42:50 +0100

    styling location

--- src/components/Location/Location.js
+++ src/components/Location/Location.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import axios from 'axios'
 
+import './location.css'
+
 class Location extends React.Component{
 
   constructor(){
@@ -22,18 +24,40 @@ class Location extends React.Component{
     console.log(universesArray)
 
     const planets = universesArray?.map((singlePlanet, index) =>{
-      return <div key={index}>
-        <img src={
-          singlePlanet.name === 'Earth (C-137)' ? "https://i.ibb.co/hc4VJ11/Planet-Earth.png" 
+      return <div className="planet-container" key={index}>
+        <div className="flex-container">
+          
+          <img className="planet-image" src={
+          singlePlanet.name === 'Earth (C-137)' ? "https://i.ibb.co/Y2ZzrRt/Earth-C-137.png" 
           : singlePlanet.name === 'Abadango' ? "https://i.ibb.co/CWdKqfd/abadango-Cluster.jpg" 
           : singlePlanet.name === 'Citadel of Ricks' ?  "https://i.ibb.co/vJFhXsb/citadel-Of-Ricks.png" 
           : singlePlanet.name === "Worldender's lair" ? "https://i.ibb.co/p4xZdGb/Worldenders-Lair.png" 
           : singlePlanet.name === 'Anatomy Park' ? "https://i.ibb.co/tCggS4c/Anatomy-Park.png" 
-          : singlePlanet.name === 'Interdimensional Cable' ? "https://i.ibb.co/9g9XD3Z/Interdimensional-Cable.jpg" : "https://i.ibb.co/9g9XD3Z/Interdimensional-Cable.jpg"
-        } alt="planets"/>
-        <h1>Name: {singlePlanet.name}</h1>
-        <h2>Type: {singlePlanet.type}</h2>
+          : singlePlanet.name === 'Interdimensional Cable' ? "https://i.ibb.co/9g9XD3Z/Interdimensional-Cable.jpg" 
+          : singlePlanet.name === 'Immortality Field Resort' ? "https://i.ibb.co/CtRB3Jm/Immortality-Field-Resort.png" 
+          : singlePlanet.name === 'Post-Apocalyptic Earth' ? "https://i.ibb.co/68RzCWb/Post-Apocalyptic-World.png" 
+          : singlePlanet.name === 'Purge Planet' ? "https://i.ibb.co/ZLhq2V7/purge-planet.png" 
+          : singlePlanet.name === 'Venzenulon 7' ? "https://i.ibb.co/DKSNZnQ/Venzenulon-7.png" 
+          : singlePlanet.name === 'Bepis 9' ? "https://i.ibb.co/cJVFggg/Bepis-9.jpg" 
+          : singlePlanet.name === 'Cronenberg Earth' ? "https://i.ibb.co/L84w5KD/Cronenberg-Earth.png" 
+          : singlePlanet.name === 'Nuptia 4' ? "https://i.ibb.co/58DZ50X/Nuptia4.png" 
+          : singlePlanet.name === "Giant's Town" ? "https://i.ibb.co/9TKd6d6/Giant-s-Town.png" 
+          : singlePlanet.name === "Bird World" ? "https://i.ibb.co/GTzFPYc/Bird-World.png" 
+          : singlePlanet.name === "St. Gloopy Noops Hospital" ? "https://i.ibb.co/3RdvMB4/St-Gloopy-Noops-Hospital.png" 
+          : singlePlanet.name === "Earth (5-126)" ? "https://i.ibb.co/JcWGZvm/Earth-5-126.gif" 
+          : singlePlanet.name === "Mr. Goldenfold's dream" ? "https://i.ibb.co/chgY7nj/Mr-Goldenfold-s-dream.jpg" 
+          : singlePlanet.name === "Gromflom Prime" ? "https://i.ibb.co/nn4DdD8/Gromflom-Prime.png" 
+          : singlePlanet.name === "Earth (Replacement Dimension)" ? "https://i.ibb.co/XFTyxPs/Earth-Replacement-Dimension.png"
+          : singlePlanet.name === !singlePlanet.name ? "https://i.ibb.co/N9DbGZY/No-Image.png" : "https://i.ibb.co/N9DbGZY/No-Image.png"
+        } alt={singlePlanet.name}/> 
+        <h1 className="planet-name" >Name: {singlePlanet.name}</h1>
+        <h2 className="planet-type">Type: {singlePlanet.type}</h2>
         <h2>Dimension: {singlePlanet.dimension}</h2>
+        <h2 className="planet-name">Population: {singlePlanet.residents.length}</h2>
+          
+        
+        </div>
+
       </div>
     })
 
@@ -44,4 +68,5 @@ class Location extends React.Component{
 }
 
 export default Location
+
 
--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -10,7 +10,7 @@ class MainPage extends Component {
               <div className='intro'>
                 <h1>Welcome to the Rick and Morty Universe</h1>
                 <h2>Click the portal for a interdimentional trip!</h2>
-                <Link to={'/location'}><img src="https://media.giphy.com/media/i2tLw5ZyikSFdkeGHT/giphy.gif" alt="portal"/></Link>
+                <Link to={'/location'}><img className="portal-image" src="https://media.giphy.com/media/i2tLw5ZyikSFdkeGHT/giphy.gif" alt="portal"/></Link>
               </div>
                 {/* <h1>Jerry is mad at Rick because Rick took Morty without permission to another adventure. Now we have to find Morty!</h1> */}
                 <div className='main-page-main'></div>
